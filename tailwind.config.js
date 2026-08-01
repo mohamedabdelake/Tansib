6255af51691a2e3bc5231637d58bea220759b862
ســونـيــڪ ءل غــداࢪࢪ <166135545+mohamedabdelake@users.noreply.github.com>
2026-08-01 01:16:34 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        cyber: '#030712',
+        neonBlue: '#00f0ff',
+      },
+      animation: {
+        'glow-pulse': 'glow-pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+      },
+      keyframes: {
+        'glow-pulse': {
+          '0%, 100%': { opacity: '1' },
+          '50%': { opacity: '.5' },
+        }
+      }
+    },
+  },
+  plugins: [],
+}
